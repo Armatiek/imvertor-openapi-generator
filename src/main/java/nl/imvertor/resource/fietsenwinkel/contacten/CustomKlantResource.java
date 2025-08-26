dd0e2155cd5451c3e58e5ab5f72619d8a68796dc
Maarten Kroon <maarten.kroon@armatiek.nl>
2025-08-26 18:17:14 +0200

    Added CustomKlantResource; changed to pom

---
+++ src/main/java/nl/imvertor/resource/fietsenwinkel/contacten/CustomKlantResource.java
@@ -0,0 +1,69 @@
+package nl.imvertor.resource.fietsenwinkel.contacten;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
+import nl.imvertor.model.fietsenwinkel.contacten.PaginatedKlantList;
+
+@Path("/klant")
+@Tag(name = "Klant", description = "Een persoon die een fiets heeft gekocht.")
+public class CustomKlantResource {
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Get all Klant objects filtered by name and/or address", description = "Retrieves a paginated list of all Klant objects filtered by name and/or address")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "OK",
+      content = @Content(mediaType = "application/json",
+      schema = @Schema(implementation = PaginatedKlantList.class))),
+    @ApiResponse(responseCode = "400", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/400"),
+    @ApiResponse(responseCode = "401", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/401"),
+    @ApiResponse(responseCode = "403", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/403"),
+    @ApiResponse(responseCode = "409", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/409"),
+    @ApiResponse(responseCode = "410", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/410"),
+    @ApiResponse(responseCode = "415", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/415"),
+    @ApiResponse(responseCode = "429", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/429"),
+    @ApiResponse(responseCode = "500", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/500"),
+    @ApiResponse(responseCode = "501", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/501"),
+    @ApiResponse(responseCode = "503", ref="https://raw.githubusercontent.com/VNG-Realisatie/API-Kennisbank/master/common/common.yaml#/components/responses/503")
+  })
+  public Response getKlantenByNameAndAddress(
+    @QueryParam("page")
+    @DefaultValue("0")
+    @Parameter(description = "Page number (0-based)", example = "0")
+    @Min(0) int page,
+
+    @QueryParam("size")
+    @DefaultValue("20")
+    @Parameter(description = "Number of items per page", example = "20")
+    @Min(1) int size,
+
+    @QueryParam("sort")
+    @DefaultValue("id")
+    @Parameter(description = "Field to sort by", example = "name")
+    String sortBy,
+    
+    @QueryParam("naam")
+    @Parameter(description = "Name of the Klant", example = "Johan")
+    String name,
+    
+    @QueryParam("address")
+    @Parameter(description = "Address of the Klant", example = "Dorpsstraat 1")
+    String address) {
+    return Response.ok().build();
+  }
+
+}
